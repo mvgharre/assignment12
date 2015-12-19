56c3290e3ef3c4319f5ce51ab577985ab3142779
hari prasad <mvgharre@gmail.com>
2015-12-18 23:13:17 -0600

    first commit

---
+++ app/src/main/java/com/example/hariprasad/fragments/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.hariprasad.fragments;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+
+    public void selectFrag(View view) {
+        Fragment fr;
+
+        if(view == findViewById(R.id.button2)) {
+            fr = new FragmentTwo();
+
+        }else {
+            fr = new FragmentOne();
+        }
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_place, fr);
+        fragmentTransaction.commit();
+
+    }
+
+}
